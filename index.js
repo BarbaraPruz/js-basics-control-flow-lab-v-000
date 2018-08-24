2e8acba49133e0071e02ba7b170ffa2d317d28d8
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-24 20:40:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code in this file!
 function scuberGreetingForFeet ( feet ) {
-  if (feet < 200) 
+  if (feet < 200)
     return 'This one is on me!';
-  else if (feet > 2500 ) {
+  else if (feet > 2500 ) 
     return 'No can do.';
   else
     return `I will gladly take your thirty bucks.`;
