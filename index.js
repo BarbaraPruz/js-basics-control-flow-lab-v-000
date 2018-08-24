6a3ce65d823550164cf617317bfa559efee88c48
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-24 20:42:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 function scuberGreetingForFeet ( feet ) {
   if (feet < 200)
     return 'This one is on me!';
-  else if (feet > 2500 ) 
+  else if (feet > 2500 )
     return 'No can do.';
   else
     return `I will gladly take your thirty bucks.`;
+}
+
+function ternaryCheckCity (city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go." ;
 }
